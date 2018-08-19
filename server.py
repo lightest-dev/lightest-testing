31824ef4590c79c6b3787cd88b3f349b60dcf4c0
Andriy Vasylyk <deadsith@hotmail.com>
2018-08-19 20:09:39 +0300

    Continued implemeting checker

--- server.py
+++ server.py
@@ -2,12 +2,16 @@ import socket
 import sys
 import requests
 import time
+import os
+import shutil
 
 api_server = ""
 max_tries = 5
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 10000)
 sock.bind(server_address)
+code_upload = ""
+extension = ""
 
 sock.listen(1)
 
@@ -38,7 +42,7 @@ def read_data(connection):
                 toread = int.from_bytes(view[:4], signed=True)
                 type = view[4]
                 f.write(view[5:length])
-                toread = toread + 5 - length
+                toread = toread + 1 - length
             f.write(view)
             toread = toread - length
         else:
@@ -51,8 +55,24 @@ def process_data(type):
     # name:{filename}
     if type == 1:
         str = ""
+        with open('tempfile', 'r') as myfile:
+            str = myfile.read().replace('\n', '')
+        if str.startswith("name:"):
+            filename = str[5:]
+        elif str.startswith("code_upload:"):
+            global code_upload
+            if code_upload:
+                shutil.rmtree(code_upload, True)
+            code_upload = str[12:]
+            os.mkdir(code_upload, 700)
+        elif str.startswith("code"):
+            filename = str
+            _, file_extension = os.path.splitext(str)
+            global extension
+            extension = file_extension
+            check(filename)
     elif type == 2:
-        binary = ""
+        os.rename("tempfile", os.path.join(code_upload, filename))
 
 
 def compile(path):
@@ -60,6 +80,10 @@ def compile(path):
 
 
 def check(path):
+    if not compile(path):
+        # send compilation error
+        return
+    # check files
     return
 
 
