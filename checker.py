522cf2bb52b4b9b1d1ae5202d29e8d31b3588ab3
Andriy Vasylyk <deadsith@hotmail.com>
2019-01-02 22:19:48 +0200

    Fixed errors in checker
    Added defult checker

--- checker.py
+++ checker.py
@@ -7,7 +7,7 @@ class code_checker():
     def __init__(self, checker_folder):
         self.checker_folder = checker_folder
         self.output_log_file = 'run.log'
-        self.output_file = 'temp.out'
+        self.output_file = './temp.out'
         self.separator = '--------------------------------------------------------------------------------'
 
     def __run_command__(self, command, time, memory=None, file_count=None, stdin=None):
@@ -23,7 +23,7 @@ class code_checker():
         try:
             result = executor.execute_run(
                 args, self.output_log_file, memlimit=memory, hardtimelimit=time, files_count_limit=file_count, stdin=stdin)
-            if result['cputime'] is not None and result['cputime'] > time:
+            if 'cputime' in result and result['cputime'] > time:
                 result['terminationreason'] = 'cputime'
         except:
             result = {'terminationreason': 'something went very wrong'}
@@ -64,11 +64,11 @@ class code_checker():
                 return result
             test_result = self.__run_test__(
                 checker_path, filename, output_file, memory, time)
-            if test_result['error'] is not None:
+            if 'error' in test_result:
                 if test_result['error'] == 'Checker is not working':
                     return test_result
                 failed_tests = failed_tests + 1
-            elif test_result['code'] is not None and test_result['code'] == 0:
+            elif 'code' in test_result and test_result['code'] == 0:
                 passed_tests = passed_tests + 1
             else:
                 failed_tests = failed_tests + 1
@@ -81,7 +81,7 @@ class code_checker():
     def __run_test__(self, checker, input_path, output_path, memory, time):
         with open(input_path, "r") as input:
             result = self.__run_command__(
-                "compiled.run", time,
+                "./compiled.run", time,
                 memory=memory, file_count=1, stdin=input)
         result_exists = self.__parse_log__()
         if not result_exists:
@@ -90,17 +90,18 @@ class code_checker():
         # check if run is ok
         result = self.__run_command__(" ".join([checker, input_path, self.output_file, output_path]), time,
                                       memory=memory, file_count=0)
-        if result['terminationreason'] is not None:
+        if 'terminationreason' in result:
             result = {'error': 'Checker is not working'}
         else:
             result = {'code': result['exitcode']}
         return result
 
     def compile_checker(self, name):
-        path = os.path.join(self.checker_folder, name)
-        result = self.__run_command__("g++ -I" + self.checker_folder + " -O2 -std=c++11 " + path +
-                                      " -o " + name + ".run", 0, 30000)
-        if result['terminationreason'] is not None or result['exitcode'] != 0:
+        path = os.path.join(self.checker_folder, name + '.cpp')
+        result_path = os.path.join(self.checker_folder, name + '.run')
+        result = self.__run_command__("g++ -I " + self.checker_folder + " -O2 -std=c++11 " + path +
+                                      " -o " + result_path, 30000)
+        if 'terminationreason' in result or result['exitcode'] != 0:
             result = {'error': 'Checker is not compiling'}
         return result
 
---
+++ testing/sum_console.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    cout << b - a;
+
+    return 0;
+}
