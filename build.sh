96799d525af222711e154237eef46066ca20b6f8
Andriy Vasylyk <deadsith@hotmail.com>
2019-01-27 22:15:49 +0200

    Added travis build file
    This fixes #13

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 git clone https://github.com/MikeMirzayanov/testlib.git
 mkdir dist
 cp ./{checker.py,hasher.py,main.py,server.py,__init__.py,settings.json} ./dist
-
+echo $API_SERVER
 sed -i "s|api_server|${API_SERVER}|" ./dist/settings.json
 cp -a ./models ./dist
 mkdir ./dist/checkers
 cp ./testlib/testlib.h ./dist/checkers/
+docker build -t deadsith/lightest-testing
