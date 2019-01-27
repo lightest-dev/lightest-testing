872af5485a47052c7207ffdb921e2dc035cfe803
Andriy Vasylyk <deadsith@hotmail.com>
2019-01-27 22:23:58 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@ git clone https://github.com/MikeMirzayanov/testlib.git
 mkdir dist
 cp ./{checker.py,hasher.py,main.py,server.py,__init__.py,settings.json} ./dist
 echo $API_SERVER
+echo $VERSION
 sed -i "s|api_server|${API_SERVER}|" ./dist/settings.json
 cp -a ./models ./dist
 mkdir ./dist/checkers
 cp ./testlib/testlib.h ./dist/checkers/
-docker build -t deadsith/lightest-testing
+docker build -t deadsith/lightest-testing .
